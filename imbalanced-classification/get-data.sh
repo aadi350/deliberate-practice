e1305195900678c46cbde29afca73c7b81e9f126
aadi350 <a.aadi380@gmail.com>
2021-12-25 14:54:50 -0400

    move everything to notebbook

--- imbalanced-classification/get-data.sh
+++ imbalanced-classification/get-data.sh
@@ -1,7 +1,7 @@
 wget http://kdd.ics.uci.edu/databases/kddcup99/task.html -O task.html
 mkdir data
-gunzip data/kddcup.data.gz 
 wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz -O data/kddcup.data.gz 
 wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.names -O data/kddcup.names
 wget http://kdd.ics.uci.edu/databases/kddcup99/kddcup.names  -O data/kddcup.names
 wget http://kdd.ics.uci.edu/databases/kddcup99/training_attack_types -O data/training_attack_types
+gunzip data/kddcup.data.gz 
